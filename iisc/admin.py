43bed9fbca1abe00a0929f2e4d0e6bd2cef3c402
arya <aboudaie@brandeis.edu>
2016-02-25 13:26:31 +0530

    admin more descriptive for fiducial type

--- iisc/admin.py
+++ iisc/admin.py
@@ -5,5 +5,9 @@ from .models import Event, Professor, Lab, FidType
 admin.site.register(Event)
 admin.site.register(Professor)
 admin.site.register(Lab)
-admin.site.register(FidType)
+
+@admin.register(FidType)
+class FidTypeAdmin(admin.ModelAdmin):
+    list_display = ('fiducial_number', 'model_type')
+
 
