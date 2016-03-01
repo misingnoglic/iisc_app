5549129e79c1b2c1bb8083c8d17e5de5f207a0df
arya <aboudaie@brandeis.edu>
2016-03-01 14:28:52 +0530

    fixed bug regarding the saving of fiducial objects

--- iisc/models.py
+++ iisc/models.py
@@ -19,7 +19,6 @@ class FiducialObject(models.Model):
         abstract = True
 
     def save(self, *args, **kwargs):
-        #import pdb ; pdb.set_trace()
         # If it's being created for the first time - check to make sure the fn doesn't exist
         # If it's being updated - If the fn is the same don't worry, else:
             # Make sure the new fn doesn't exist, but otherwise update the old FidType
@@ -30,15 +29,20 @@ class FiducialObject(models.Model):
                     code='existingfn',
                     params={'fiducial_number': self.fiducial_number})
 
+        if self.pk:
+            old_fn = Model.objects.get(pk=self.pk).fiducial_number  # The old fn from the database
+        else:
+            old_fn = self.fiducial_number
+
         # If this fn already exists, throw an error
-        if FidType.objects.filter(fiducial_number=self.fiducial_number):
-            raise duplicate_fn_error
+        if self.pk and FidType.objects.filter(fiducial_number=self.fiducial_number):
+            if old_fn != self.fiducial_number:
+                raise duplicate_fn_error
 
         if not self.pk: # If it is created for the first time - save the record
             record = FidType(fiducial_number=self.fiducial_number, model_type=self._meta.object_name)
             record.save()
         else:
-            old_fn = Model.objects.get(pk=self.pk).fiducial_number  # The old fn from the database
             if old_fn != self.fiducial_number:  # If the fn is updated
                 f = FidType.objects.get(fiducial_number=old_fn)  # update the number in the FidType object & save
                 f.fiducial_number = self.fiducial_number
@@ -47,7 +51,6 @@ class FiducialObject(models.Model):
                 pass
 
         super(FiducialObject, self).save(*args, **kwargs)  # Call the "real" save() method.
-
 
 
 class Event(FiducialObject):
@@ -73,7 +76,10 @@ class Lab(FiducialObject):
     room = models.IntegerField()
     name = models.CharField(max_length=20)
     description = models.TextField()
-    faculty_photo = models.ImageField(upload_to="static/photos/", blank=True)
-    video = models.FileField(upload_to="static/photos/", blank=True)
+    faculty_photo = models.ImageField(upload_to="media/", blank=True)
+    video = models.FileField(upload_to="media/", blank=True)
+
+    def __unicode__(self):
+        return self.name
 
 #class Participant(models.Model):
